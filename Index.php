337cfc08f224bf289b32719ffe13f3cd1b45866b
AJ Fite <goldman60@gmail.com>
2013-01-30 23:26:58 -0800

    First Commit
    Using CodeIgniter 2

---
+++ Index.php
@@ -0,0 +1,17 @@
+<?php #temporary ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>NCLF Minecraft - Maitenance</title>
+</head>
+<body>
+<h1>NCLF Minecraft</h1>
+<p>Pardon the dust I am rebuilding the website</p>
+<p>Until it is finished the following links should be of interest</p>
+<ul>
+	<li><a href="/map">Creative Server Map</a></li>
+	<li><a href="/ftbmap">Feed the Beast Server Map</a></li>
+	<li><a href="https://webchat.esper.net/?nick=&channels=NCLF">IRC Chat (New! Chat with both servers!)</a></li>
+</ul>
+</body>
+</html>
