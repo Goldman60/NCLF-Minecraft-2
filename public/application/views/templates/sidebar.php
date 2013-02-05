4567349773f4895b5a4c21b7e096ad62b7b97d88
AJ Fite <goldman60@gmail.com>
2013-02-05 03:17:48 -0800

    Begin work on server Query

--- public/application/views/templates/sidebar.php
+++ public/application/views/templates/sidebar.php
@@ -1,9 +1,11 @@
 <div id="sidebar">
 <h2>Server Status</h2>
-<p class="serverstats">
-<h3>Creative</h3>
-</p>
-<p class="serverstats">
-<h3>FTB</h3>
-</p>
+<div class="serverstats">
+	<h3>Creative</h3>
+	<p class="onlinecount">X/X Players Online</p>
+</div>
+<div class="serverstats">
+	<h3>FTB</h3>
+	<p class="onlinecount">X/X Players Online</p>
+</div>
 </div>
