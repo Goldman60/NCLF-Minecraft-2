47367c5aa4afb52ab702da184c846332cf13def8
AJ Fite <goldman60@gmail.com>
2013-02-05 02:17:31 -0800

    Navigation work, removing more debug

--- public/application/views/templates/footer.php
+++ public/application/views/templates/footer.php
@@ -1,6 +1,6 @@
 <footer id="footer">
-<a id="NCLFNetwork">Part of the NCLF Network</a><br />
-<span id="copynotice">&copy; 2010-2013 AJ Fite and the Nerd Cave Liberation Front.  All Rights Reserved.</span>
+<a id="NCLFNetwork" href="http://nclf.net">Part of the <abbr title="Nerd Cave Liberation Front">NCLF</abbr> Network</a><br />
+<span id="copynotice">&copy; 2010-2013 <a href="http://ajfite.com">AJ Fite</a> and the Nerd Cave Liberation Front.  All Rights Reserved.</span>
 </footer>
 </body>
 </html>
--- public/application/views/templates/sidebar.php
+++ public/application/views/templates/sidebar.php
@@ -1,3 +1,9 @@
 <div id="sidebar">
 <h2>Server Status</h2>
+<p class="serverstats">
+<h3>Creative</h3>
+</p>
+<p class="serverstats">
+<h3>FTB</h3>
+</p>
 </div>
