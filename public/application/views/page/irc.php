095319d4d59b5e1dca12457d2255fb13286991d4
AJ Fite <goldman60@gmail.com>
2013-02-11 01:30:39 -0800

    Fix a:visited

--- public/application/views/page/irc.php
+++ public/application/views/page/irc.php
@@ -1,4 +1,4 @@
-<h2><abbr title="Internet Relay Chat">IRC</abbr></h2>
+<h2>Server Chat (<abbr title="Internet Relay Chat">IRC</abbr>)</h2>
 <iframe src="https://webchat.esper.net/?nick=&amp;channels=NCLF" width="797" height="500">iFrames must be enabled to use embedded webchat.</iframe>
 <h3>Commands</h3>
 <ul>
