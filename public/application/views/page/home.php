40e30e7a7784aef5e828472528bddac0af63ad09
AJ Fite <goldman60@gmail.com>
2013-02-12 01:43:35 -0800

    TYPOS AHOY

--- public/application/views/page/home.php
+++ public/application/views/page/home.php
@@ -1,2 +1,2 @@
 <h2>Home</h2>
-<p>NCLF Minecraft hosts two private minecraft servers, these servers are protected and run on greylists/whitelists.</p>
+<p>NCLF Minecraft hosts two private Minecraft servers, these servers are protected and run on greylists/whitelists.</p>
--- public/application/views/page/survival.php
+++ public/application/views/page/survival.php
@@ -3,7 +3,7 @@
 <p>Feed the Beast is a collection of mods, the client is available for download <a href="http://www.feed-the-beast.com/#download_wrapper">here</a>.</p>
 <p>We use "Feed The Beast Beta Pack A (v12)" selectable in the launcher.</p>
 <h3>Server Info</h3>
-<p>This server is pure hard mode survival</p>
+<p>This server is pure hard mode survival, no exceptions.</p>
 <p>Connect at <code>nclf.net:25566</code></p>
 <h3>Map</h3>
 <p><a href="http://nclfminecraft.org/ftbmap/">Dynmap</a></p>
