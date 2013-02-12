491aba77b9f671b9ba85d0745d3ed0dcddb79601
AJ Fite <goldman60@gmail.com>
2013-02-12 01:34:50 -0800

    Renamed FTB to Survival

---
+++ public/application/views/page/survival.php
@@ -0,0 +1,9 @@
+<h2>Survival</h2>
+<h3>Client Info</h3>
+<p>Feed the Beast is a collection of mods, the client is available for download <a href="http://www.feed-the-beast.com/#download_wrapper">here</a>.</p>
+<p>We use "Feed The Beast Beta Pack A (v12)" selectable in the launcher.</p>
+<h3>Server Info</h3>
+<p>This server is pure hard mode survival</p>
+<p>Connect at <code>nclf.net:25566</code></p>
+<h3>Map</h3>
+<p><a href="http://nclfminecraft.org/ftbmap/">Dynmap</a></p>
